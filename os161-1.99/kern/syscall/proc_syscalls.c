ab2e6bbd0c759fd1d45214422ee8e1f3f2fc4f20
unknown <zc3huang@uwaterloo.ca>
2022-02-14 15:18:18 -0500

    add sys_fork debug

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -106,7 +106,7 @@ sys_fork(pid_t* retval, struct trapframe *tf)
   struct trapframe* trapframe_for_child = kmalloc(sizeof(struct trapframe));
   trapframe_for_child = tf;
 
-  thread_fork("child_thread", child, enter_forked_process, tf, 0);
+  thread_fork("child_thread", child, &enter_forked_process, tf, 0);
 
   *retval = 0;
   return 0;
