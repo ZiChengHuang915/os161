a4b67f6c033e11ca1dd3749e75e631e845154b54
unknown <zc3huang@uwaterloo.ca>
2022-02-15 17:17:52 -0500

    exit debug

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -65,7 +65,7 @@ void sys__exit(int exitcode) {
      will wake up the kernel menu thread */
 #if OPT_A1
   spinlock_acquire(&p->p_lock);
-  if (p->p_parent->p_exitstatus == RUNNING) {
+  if (p->p_parent != NULL) {
     p->p_parent = NULL;
     p->p_exitstatus = EXITED;
     p->p_exitcode = _MKWAIT_EXIT(exitcode);
